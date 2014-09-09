203f79786d722b92fe559c323d863166d7a3f2f9
marshalstar <marshal.starcraft@gmail.com>
2014-09-08 22:59:35 -0300

    estilo feito do jeito porco
    não baixei as bibliotecas;
    apenas linkei elas ao original (alguém tem que baixer e arrumar os
    arquivos de footer e header)

--- app/views/templates/partials/footer.blade.php
+++ app/views/templates/partials/footer.blade.php
@@ -2,5 +2,8 @@
     <script src="https://code.angularjs.org/1.2.8/angular-route.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="/javascript/rails.js"></script>
+    <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+    <script src="http://getbootstrap.com/assets/js/docs.min.js"></script>
 </body>
 </html>
