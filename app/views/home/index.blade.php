4b4ff072fa39a8f54cfe79bd1d87c81acba9349f
guigorges <guilherme.gorges@hotmail.com>
2014-09-12 20:25:24 -0300

    Mexendo na página inicial

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -12,8 +12,6 @@
     }
     #layer1 {
         position: fixed;
-        /*background: url("http://upload.wikimedia.org/wikipedia/commons/6/60/Green_Park,_London_-_April_2007.jpg") no-repeat;*/
-        background: url("http://upload.wikimedia.org/wikipedia/commons/6/60/Green_Park,_London_-_April_2007.jpg") no-repeat;
         width: 100%;
         height: 2000px;
         top: 0;
@@ -27,115 +25,32 @@
         width: 100%;
     }
 </style>
+    <body>
+    <header class = "jumbotron subhead">
+        <div class = "container">
+            <h1>Listonly</h1>
+            <p>Criação de Checklists</p>
+        </div>
+    </header>
 
-<div id="layer1"></div>
-<div id="main" class="container theme-showcase">
-
-    <lu>
-        <li>A</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-        <li>a</li>
-    </lu>
-    
-<!--    <div id="preload">-->
-<!--        <img src="http://upload.wikimedia.org/wikipedia/commons/6/60/Green_Park,_London_-_April_2007.jpg">-->
-<!--    </div>-->
-
-<!--    <section id="slide-1" class="homeSlide">-->
-<!--        <div class="bcg"-->
-<!--             data-center="background-position: 50% 0px;"-->
-<!--             data-top-bottom="background-position: 50% -100px;"-->
-<!--             data-anchor-target="#slide-1"-->
-<!--            >-->
-<!--            <div class="hsContainer">-->
-<!--                <div class="hsContent"-->
-<!--                     data-center="bottom: 200px; opacity: 1"-->
-<!--                     data-top="bottom: 1200px; opacity: 0"-->
-<!--                     data-anchor-target="#slide-1 h2"-->
-<!--                    >-->
-<!---->
-<!--                    <a href="{{ URL::route('alternatives.index') }}">{{ Str::title(Lang::get('alternativas')) }}</a>-->
-<!--                    <a href="{{ URL::route('checklists.index') }}">{{ Str::title(Lang::get('checklists')) }}</a>-->
-<!--                    <a href="{{ URL::route('evaluations.index') }}">{{ Str::title(Lang::get('avaliações')) }}</a>-->
-<!--                    <a href="{{ URL::route('questions.index') }}">{{ Str::title(Lang::get('questões')) }}</a>-->
-<!--                    <a href="{{ URL::route('tags.index') }}">{{ Str::title(Lang::get('tags')) }}</a>-->
-<!--                    <a href="{{ URL::route('titles.index') }}">{{ Str::title(Lang::get('títulos')) }}</a>-->
-<!--                    <a href="{{ URL::route('types.index') }}">{{ Str::title(Lang::get('tipos')) }}</a>-->
-<!--                    <a href="{{ URL::route('users.index') }}">{{ Str::title(Lang::get('usuários')) }}</a>-->
-<!---->
-<!--                    <p>larastonly1</p>-->
-<!--                    <p>larastonly2</p>-->
-<!--                    <p>larastonly3</p>-->
-<!--                    <p>larastonly4</p>-->
-<!--                    <p>larastonly5</p>-->
-<!--                    <p>larastonly6</p>-->
-<!--                    <p>larastonly7</p>-->
-<!--                    <p>larastonly8</p>-->
-<!--                    <p>larastonly9</p>-->
-<!---->
-<!--                    <p>http://getbootstrap.com/javascript/#scrollspy</p>-->
-<!---->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </section>-->
+<div class="container">
+    <div class="row">
+        <aside class="span8">
+            <ul class="nav">
+                <li><a href="{{URL::route('alternatives.index')}}"><i class="icon-chevron-right"></i> Gerenciar Alternativas</li></a>
+                <li><a href="{{URL::route('questions.index')}}"><i class="icon-chevron-right"></i> Gerenciar Questões</li></a>
+                <li><a href="{{URL::route('checklists.index')}}"><i class="icon-chevron-right"></i> Gerenciar Checklist </li></a>
+                <li><a href="{{URL::route('evaluations.index')}}"><i class="icon-chevron-right"></i>Gerenciar Avaliações</li></a>
+                <li><a href="{{URL::route('tags.index')}}"><i class="icon-chevron-right"></i>Gerenciar Tags </li></a>
+                <li><a href="{{URL::route('titles.index')}}"><i class="icon-chevron-right"></i>Gerenciar Títulos </li></a>
+                <li><a href="{{URL::route('types.index')}}"><i class="icon-chevron-right"></i>Gerenciar Tipo </li></a>
+                <li><a href="{{URL::route('users.index')}}"><i class="icon-chevron-right"></i>Gerenciar Perfil</li></a>
+            </ul>
+        </aside>
+    </div>
 </div>
+    </body>
 
-<script>
-    function parallax() {
-        var layer1 = document.getElementById("layer1");
-        layer1.style.top = -(window.pageYOffset /4)+'px';
-    }
-    window.addEventListener("scroll", parallax, false);
-</script>
+
 
 @stop
