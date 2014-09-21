ae2fd953e3f3c843905d1631521fc8f2285cb9f8
marshalstar <marshal.starcraft@gmail.com>
2014-09-20 21:26:54 -0300

    gorges, brigadão pelo merge grátis!

--- app/controllers/TypesController.php
+++ app/controllers/TypesController.php
@@ -3,32 +3,17 @@
 class TypesController extends Controller
 {
 
-	/**
-	 * Lista types
-	 *
-	 * @return Response
-	 */
 	public function index()
     {
 		$types = Type::all();
 		return View::make('types.index', compact('types'));
 	}
 
-	/**
-	 * Mostra formulário de cadastro espefíco de type
-	 *
-	 * @return Response
-	 */
 	public function create()
     {
 		return View::make('types.create');
 	}
 
-	/**
-	 * Salva type espefíco no banco
-	 *
-	 * @return Response
-	 */
 	public function store()
     {
         $type = new Type();
@@ -38,36 +23,18 @@ class TypesController extends Controller
         return Redirect::route('types.create')->withErrors($type->errors());
 	}
 
-	/**
-	 * Mostra type específico.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($id)
     {
 		$type = Type::findOrFail($id);
 		return View::make('types.show', compact('type'));
 	}
 
-	/**
-	 * Mostra formulário de edição espefícico de type.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function edit($id)
     {
 		$type = Type::find($id);
 		return View::make('types.edit', compact('type'));
 	}
 
-	/**
-	 * Atualiza type específico no banco
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id)
     {
         $type = Type::find($id);
@@ -78,12 +45,6 @@ class TypesController extends Controller
         return Redirect::route('types.edit')->withErrors($type->errors());
 	}
 
-	/**
-	 * Remove type específico do banco.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
     {
 		Type::destroy($id);
