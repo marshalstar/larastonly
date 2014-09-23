f408ff3164185897114a591d8544e739d71856e9
marshalstar <marshal.starcraft@gmail.com>
2014-09-23 20:37:40 -0300

    [!C]

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -55,4 +55,14 @@ class DatabaseSeeder extends Seeder {
         $this->call('AnswersTableSeeder');
     }
 
+    private function massiveSeeder()
+    {
+        $queryUser = "INSERT INTO 'users' ('username', 'email', 'password') VALUES ('user', 'email', 'password')";
+        foreach (range(1, 1000) as $i) {
+
+        }
+
+        DB::statement("");
+    }
+
 }
