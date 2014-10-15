eff0bd3bd53f6ec1e747cc84637b18b43c1630c0
guigorges <guilherme.gorges@hotmail.com>
2014-10-15 20:09:13 -0300

    [!C]

--- app/controllers/PDFController.php
+++ app/controllers/PDFController.php
@@ -3,8 +3,6 @@ class PDFController extends BaseController
 {
 	public function imprimir()
 	{
-		$pdf = new Pdf;
-		$pdf->addPage('http://localhost:8000/checklists/nyw');
-		$pdf->send();
+		
 	}
 }
