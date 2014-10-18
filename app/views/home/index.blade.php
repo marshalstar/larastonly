2024e370d10baf39f1ed9786a9e6f4c041126de5
guigorges <guilherme.gorges@hotmail.com>
2014-10-18 02:41:48 -0300

    Inserindo icones

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -8,7 +8,7 @@
         <div class="alert alert-info">{{ Session::get('message') }}</div>
     @endif
 
-    <h1>Listonly</h1>
+    <h1><i class="fa fa-home"> Listonly</i></h1>
 
     <div class="panel">
 
@@ -16,24 +16,24 @@
             <p> Bem-vindo, {{Auth::user()->username}}</p>
             <ul>
               
-            <li><a href="{{URL::route('users.logout')}}"> <i class="icon-chevron-right"></i> Sair </a></li>
-            <li><a href="{{URL::route('changepassword')}}"> Mudar minha senha </a></li>
-            <li><a href="{{URL::route('alternatives.index')}}"><i class="icon-chevron-right"></i> Gerenciar Alternativas</a></li>
-            <li><a href="{{URL::route('questions.index')}}"><i class="icon-chevron-right"></i> Gerenciar Questões</a></li>
-            <li><a href="{{URL::route('checklists.index')}}"><i class="icon-chevron-right"></i> Gerenciar Checklist </a></li>
-            <li><a href="{{URL::route('evaluations.index')}}"><i class="icon-chevron-right"></i>Gerenciar Avaliações</a></li>
-            <li><a href="{{URL::route('tags.index')}}"><i class="icon-chevron-right"></i>Gerenciar Tags </a></li>
-            <li><a href="{{URL::route('titles.index')}}"><i class="icon-chevron-right"></i>Gerenciar Títulos </a></li>
-            <li><a href="{{URL::route('types.index')}}"><i class="icon-chevron-right"></i>Gerenciar Tipo </a></li>
-            <li><a href="{{URL::route('users.index')}}"><i class="icon-chevron-right"></i>Gerenciar Perfil</a></li>
+            <li><a href="{{URL::route('users.logout')}}"> <i class="fa fa-sign-out"></i> Sair </a></li>
+            <li><a href="{{URL::route('changepassword')}}"> <i class="fa fa-pencil-square-o"></i> Mudar minha senha </a></li>
+            <li><a href="{{URL::route('alternatives.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Alternativas</a></li>
+            <li><a href="{{URL::route('questions.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Questões</a></li>
+            <li><a href="{{URL::route('checklists.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Checklist </a></li>
+            <li><a href="{{URL::route('evaluations.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Avaliações</a></li>
+            <li><a href="{{URL::route('tags.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Tags </a></li>
+            <li><a href="{{URL::route('titles.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Títulos </a></li>
+            <li><a href="{{URL::route('types.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Tipo </a></li>
+            <li><a href="{{URL::route('users.index')}}"><i class="fa fa-pencil-square-o"></i> Gerenciar Perfil</a></li>
             </ul>
         @else
            <p> Você ainda não fez seu login.</p>
            <ul>
-           <li><a href="{{URL::route('users.login')}}"><i class="icon-chevron-right"></i>Faça seu Login</a></li>
-           <li><a href="{{URL::route('users.create')}}"><i class="icon-chevron-right"></i>Ainda não está cadastrado? Crie sua conta.</a></li>
-           <li><a href="/fb"><i class="icon-chevron-right"></i> Login via Facebook </a></li>
-           <li><a href="{{URL::route('forgot')}}"><i class="icon-chevron-right"></i> Recuperar Conta</a></li>
+           <li><a href="{{URL::route('users.login')}}"><i class="fa fa-sign-in"></i> Faça seu Login </a></li> ou <li><a href="{{URL::route('users.create')}}"><i class="fa fa-check-square-o"></i> Cadastre-se</a></li>
+           
+           <li><a href="/fb"><i class="fa fa-facebook-square"></i> Login via Facebook </a></li>
+           <li><a href="{{URL::route('forgot')}}"><i class="fa fa-refresh fa-spin"></i> Recuperar Conta</a></li>
 
            </ul>
         @endif
